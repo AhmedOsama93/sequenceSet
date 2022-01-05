8a06e97d45b23b110b0fb2d8f817965a6ca7b205
AhmedOsama93 <75946833+AhmedOsama93@users.noreply.github.com>
2022-01-05 18:39:16 +0200

    finalVersion
    this project explains how to insert, delete, merge and search in sequence set.  when data files do not fit in memory we divide them into blocks that fit in memory and it sorted too. we can improve search performance by implementing BTree to search about a specific block.

---
+++ sequenceSet/cmake-build-debug/CMakeFiles/Assignment_2Assignment_2_1.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "Assignment_2Assignment_2_1.exe"
+  "Assignment_2Assignment_2_1.exe.manifest"
+  "Assignment_2Assignment_2_1.pdb"
+  "CMakeFiles/Assignment_2Assignment_2_1.dir/main.cpp.obj"
+  "libAssignment_2Assignment_2_1.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Assignment_2Assignment_2_1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
